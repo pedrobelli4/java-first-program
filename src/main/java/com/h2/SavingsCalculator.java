f9cf734b1d3c46d1326d58950e9158ceae6980c3
pedrobelli4 <82173948+pedrobelli4@users.noreply.github.com>
2021-04-23 14:58:40 -0300

    FIM

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -4,54 +4,53 @@ import java.time.LocalDate;
 import java.time.YearMonth;
 
 public class SavingsCalculator {
-    private float [] credits;
-    private float [] debits;
+    private float[] credits;
+    private float[] debits;
 
     public SavingsCalculator(float[] credits, float[] debits) {
         this.credits = credits;
         this.debits = debits;
     }
 
-    private float sunOfCredits() {
-        float sun = 0.0f;
+    private float sumOfCredits() {
+        float sum = 0.0f;
         for(float credit: credits) {
-            sun += credit;
-
+            sum += credit;
         }
-        return sun;
+        return sum;
     }
 
-    private float sunOfDebits() {
-        float sun = 0.0f;
+    private float sumOfDebits() {
+        float sum = 0.0f;
         for(float debit: debits) {
-            sun += debit;
+            sum += debit;
         }
-        return sun;
+        return sum;
     }
 
     private static int remainingDaysInMonth(LocalDate date) {
         YearMonth yearMonth = YearMonth.of(date.getYear(), date.getMonth());
-        int totalTotalDaysInMonth = yearMonth.lengthOfMonth();
-        int remainingDays = totalTotalDaysInMonth - date.getDayOfMonth();
+        int totalDaysInMonth = yearMonth.lengthOfMonth();
+        int remainingDays = totalDaysInMonth - date.getDayOfMonth();
         return remainingDays;
     }
 
-    public float calculate () {
-        return sunOfCredits() - sunOfDebits();
+    public float calculate() {
+        return sumOfCredits() - sumOfDebits();
     }
 
-    public static void main (String[] args) {
-        final String[] credtisAsString = args[0].split(",");
+    public static void main(String[] args) {
+        final String[] creditsAsString = args[0].split(",");
         final String[] debitsAsString = args[1].split(",");
 
-        final float[] credits = new float[credtisAsString.length];
+        final float[] credits = new float[creditsAsString.length];
         final float[] debits = new float[debitsAsString.length];
 
-        for (int i = 0; i < credits.length; i++) {
-            credits[i] = Float.parseFloat(credtisAsString[i]);
+        for (int i = 0; i < creditsAsString.length; i++) {
+            credits[i] = Float.parseFloat(creditsAsString[i]);
         }
 
-        for (int i = 0; i < debits.length; i++) {
+        for (int i = 0; i < debitsAsString.length; i++) {
             debits[i] = Float.parseFloat(debitsAsString[i]);
         }
 
